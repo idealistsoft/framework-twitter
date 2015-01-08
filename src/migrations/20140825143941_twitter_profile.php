7217f4f837dcf002bc98f228b28039de0b64df23
Jared King <j@jaredtking.com>
2015-01-07 23:53:24 -0600

    use latest version of bootstrap for testing

--- src/migrations/20140825143941_twitter_profile.php
+++ src/migrations/20140825143941_twitter_profile.php
@@ -27,8 +27,8 @@ class TwitterProfile extends AbstractMigration
               ->addColumn('verified', 'boolean')
               ->addColumn('last_refreshed', 'integer')
               ->addColumn('most_recently_referenced_by', 'integer', [ 'null' => true, 'default' => null ])
-              ->addColumn('created_at', 'timestamp', ['default' => 'CURRENT_TIMESTAMP'])
-              ->addColumn('updated_at', 'timestamp', ['null' => true, 'default' => null, 'update' => 'CURRENT_TIMESTAMP'])
+              ->addColumn('created_at', 'timestamp', ['default' => 0])
+              ->addColumn('updated_at', 'timestamp', ['default' => 'CURRENT_TIMESTAMP', 'update' => 'CURRENT_TIMESTAMP'])
               ->create();
         }
     }
